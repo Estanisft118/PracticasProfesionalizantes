8f281d845e2392a0fd2a2230de2e9d19d63b103c
Estanislao Genoud <estani.isft118@gmail.com>
2025-10-29 18:06:38 -0300

    {UPDATE 11} agregando correlativas

---
+++ Backend/correlativas.js
@@ -0,0 +1,46 @@
+const enfermeria= {
+	"Enfermería Materno Infantil":[
+		"Cuidados de la Salud Centrados en la Comunidad y la Familia",
+		"Fundamentos del Cuidado",
+		"Psicología",
+		"Biología Humana"
+	],
+	"Enfermería del Adulto y del Adulto Mayor I":[
+		"Fundamentos del Cuidado",
+		"Psicología",
+		"Biología Humana"
+	],
+	"Salud Pública II":[
+		"Salud Pública I"
+	],
+	"Enfermería del Adulto y del Adulto Mayor II":[
+		"Enfermería del Adulto y del Adulto Mayor I",
+		"Farmacología en Enfermería",
+		"Nutrición y Dietoterapia"
+	],
+	"Prácticas Profesionalizantes II":[
+		"Prácticas Profesionalizantes I"
+	],
+	"Prácticas Profesionalizantes III":[
+		"Prácticas Profesionalizantes II"
+	]
+}
+// Falta finalizar y agregar de mas correlativas de otras carreras
+const contable={
+	"Contabilidad de Gestiòn":[],
+	"Matemàtica para Administraciòn":[],
+	"Matemàtica Financiera":[],
+	"Derecho Laboral":[],
+	"Derecho Comercial":[],
+	"Tecnologìas y Sistemas para la Administraciòn":[],
+	"Costos y Planificaciòn":[],
+	"Ingles II":[],
+	"Prácticas Profesionalizantes II: Diseño de Proyectos de Administraciòn":[],
+	"Administraciòn Financiera":[],
+	"Tècnica Impositiva y Laboral":[],
+	"Règimen Tributario":[],
+	"Administraciòn Estratègica":[],
+	"Gestiòn de Estados Contables":[],
+	"Finanzas de Empresas":[],
+	"Prácticas Profesionalizantes III: Gestiòn e Implementaciòn de Proyectos de Administraciòn":[]
+}
